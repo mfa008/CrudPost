e159768c028b4ac1fe6a9416ee6f4a7b092cdbde
mfa008 <fallmouha2005@gmail.com>
2025-07-30 13:04:43 +0000

    Config middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,10 +5,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/test',[PostController::class , 'test']);
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/test', [PostController::class, 'test']);
+});
